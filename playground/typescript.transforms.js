897bc5d4f6c8f782414c1e29f2839aeeed10c254
harrison.cross <harrison.cross@transport4.com>
2021-03-22 15:44:16 -0400

    Create typescript.transforms.js

---
+++ playground/typescript.transforms.js
@@ -0,0 +1,7 @@
+/*
+
+https://dev.doctorevidence.com/how-to-write-a-typescript-transform-plugin-fc5308fdd943
+
+I'd like to write a transform that lets me do something interesting at run time with TS types (validation, etc)
+
+*/

25217ce53c0ef35f4a23f67b9888ca3bff98ec18
crosshj <github@chimpjuice.com>
2021-05-30 01:17:18 -0400

    spelling mistake

--- playground/cellular-automata.js
+++ playground/cellular-automata.js
@@ -5,7 +5,7 @@ https://www.youtube.com/watch?v=9Kec_7WFyp0
 
 saw videos on the following, maybe bookmarked them, having trouble finding now
 - fruit fly embryo (similar to this - https://www.youtube.com/watch?v=B0ZlvCINozs)
-- planrian-like similation of growth and regrowth of body parts
+- planarian-like simulation of growth and regrowth of body parts
 
 what I'd like to explore:
 - even very basic cellular automata from traditional perspective

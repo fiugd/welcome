30afd0d431899f3e1f515ad2148d09918a936066
crosshj <github@chimpjuice.com>
2021-10-28 20:15:36 -0400

    theatre - it's working

--- playground/theatre/theatre.js
+++ playground/theatre/theatre.js
@@ -10,6 +10,70 @@ window.process = { env: {} };
 
 const project = Theatre.getProject('my project', { state });
 //const timeline = project.getTimeline('A timeline');
+const sheet = project.sheet('Scene');
+const shader = sheet.object('Shader', {
+	uniforms: {
+		uOpacity: 0.07000000000000006,
+		uDeepPurple: 0.74,
+		uStrength: 0,
+		uPhase: {
+			y: -0.68,
+			x: 0.31000000000000005
+		},
+		uOscilation: {
+			x: 0.45
+		},
+		uBrightness: {
+			z: -0.44000000000000006,
+			y: 0.15000000000000002
+		}
+	}
+});
 
-console.log(Object.keys(Theatre));
-console.log(Object.keys(project));
+const div = document.createElement('div');
+div.style.cssText = `
+	position: absolute;
+	width: 100px;
+	height: 100px;
+	background: white;
+`;
+document.body.append(div);
+
+shader.onValuesChange(({ uniforms }) => {
+	const { uOpacity } = uniforms;
+	const normal = Number(((uOpacity-0.045) / 0.045).toFixed(5));
+	div.style.opacity = normal;
+	div.style.left = normal * 100;
+	div.style.top = normal * 100;
+});
+
+let it;
+const play = ({ range=1, rate }) => {
+	if(typeof it === 'undefined'){
+		it = 0.1 * rate
+	}
+	const [start, end] = range;
+	requestAnimationFrame(() => {
+		let nextIt = sheet.sequence.position + it;
+		if(nextIt > end){
+			it *= -1;
+			nextIt = end + 3*it;
+		}
+		if(nextIt <= start){
+			it *= -1;
+			nextIt = start + 3*it;
+		}
+		sheet.sequence.position = nextIt;
+		requestAnimationFrame(() => play({ range, rate }));
+	});
+}
+
+project.ready.then(() => {
+	play({
+		range: [0,1.5],
+		iterationCount: 100,
+		direction: "alternate",
+		rate: 1
+	});
+});
+

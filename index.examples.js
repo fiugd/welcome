318b12027883babf26d7696ea36d3af636aecd16
crosshj <github@chimpjuice.com>
2023-02-18 23:39:22 -0500

    add diffusion uix

--- index.examples.js
+++ index.examples.js
@@ -32,6 +32,7 @@ const others = [
 	['https://crosshj.com', 'crosshj.com', 'Personal / portfolio site. More experiments here', 'https://user-images.githubusercontent.com/1816471/219903201-1e02e486-e40f-442f-9153-400930d36f0a.png'],
 	['https://crosshj.com/grfx/', 'grfx', 'Yet another graphics manipulation app', 'https://user-images.githubusercontent.com/1816471/219903128-f93a1729-7fe2-4dce-93f0-68141c6dd73e.png'],
 	['https://chimpjuice.com', 'chimpjuice.com', 'Random-ish tumblr blog', 'https://user-images.githubusercontent.com/1816471/219903174-450b51f4-7279-466f-83ad-c3811ed424b0.png'],
+	['https://crosshj.com/diffusion-uix/', 'Stable Diffusion UIX', 'An alternate user interface for Automatic1111\'s Stable Diffusion UI', 'https://user-images.githubusercontent.com/1816471/219922382-459c3178-6a4a-4c3b-9828-88e364de96d3.png'],
 ];
 
 const EnhancedStorage = ({ api }) => ({

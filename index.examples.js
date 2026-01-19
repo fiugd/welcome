e035b1d78f6d7fc66fc5ea200ef813e5574fa022
crosshj <github@chimpjuice.com>
2026-01-18 23:59:34 -0500

    Update index.examples.js

--- index.examples.js
+++ index.examples.js
@@ -39,10 +39,11 @@ const experiments = [
 const others = [
 	['https://crosshj.com', 'crosshj.com', 'Personal / portfolio site. More experiments here', 'https://user-images.githubusercontent.com/1816471/219903201-1e02e486-e40f-442f-9153-400930d36f0a.png'],
 	['https://assistant.crosshj.com/', 'Pocket Alternative', 'Bookmark your favorite websites', 'https://user-images.githubusercontent.com/1816471/219935960-2601e624-a400-4f36-99ca-dcb6679a6210.png'],
+	['https://parascene.crosshj.com/', 'parascene', 'A P2P hive of creatives and technology enthusiasts', 'https://github.com/user-attachments/assets/624a4362-1e90-4fc7-9049-a0e8b05fde82'],
     ['https://crosshj.com/grfx/', 'grfx', 'Yet another graphics manipulation app', 'https://user-images.githubusercontent.com/1816471/219903128-f93a1729-7fe2-4dce-93f0-68141c6dd73e.png'],
 	['https://crosshj.com/vermiculate/', 'Vermiculate', 'I port(ed) my favorite open source screensaver to the web', 'https://user-images.githubusercontent.com/93937172/224854477-aa3b9861-cb4f-4b92-982b-326f6847ec9a.png'],
 	['https://chimpjuice.com', 'chimpjuice.com', 'Random-ish tumblr blog', 'https://user-images.githubusercontent.com/1816471/219903174-450b51f4-7279-466f-83ad-c3811ed424b0.png'],
-	// ['https://crosshj.com/diffusion-uix/', 'Stable Diffusion UIX', 'An alternate user interface for Automatic1111\'s Stable Diffusion UI', 'https://user-images.githubusercontent.com/1816471/219922382-459c3178-6a4a-4c3b-9828-88e364de96d3.png'],
+    // ['https://crosshj.com/diffusion-uix/', 'Stable Diffusion UIX', 'An alternate user interface for Automatic1111\'s Stable Diffusion UI', 'https://user-images.githubusercontent.com/1816471/219922382-459c3178-6a4a-4c3b-9828-88e364de96d3.png'],
 ];
 
 const EnhancedStorage = ({ api }) => ({
